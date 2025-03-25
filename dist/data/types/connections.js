78e1c39b964b5c35ac6719460594e2bea615d0f0
Egil Ramsten <egil.ramsten@hotmail.com>
2025-03-25 11:10:15 +0100

    changed name on .yml file

--- dist/data/types/connections.js
+++
@@ -1,7 +0,0 @@
-export var Direction;
-(function (Direction) {
-    Direction[Direction["Field"] = 0] = "Field";
-    Direction[Direction["Road"] = 1] = "Road";
-    Direction[Direction["City"] = 2] = "City";
-    Direction[Direction["Stream"] = 3] = "Stream";
-})(Direction || (Direction = {}));
